4cf51584fba248f90c318bab371f3d7c1b7a6823
Md.Mohiuddin <saddammd203@gmail.com>
2023-04-12 23:08:48 +0600

    add area chart

--- src/components/Statatistic/Statatistic.jsx
+++ src/components/Statatistic/Statatistic.jsx
@@ -1,12 +1,30 @@
-<LineChart
-  width={400}
-  height={400}
-  data={data}
-  margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
->
-  <XAxis dataKey="name" />
-  <Tooltip />
-  <CartesianGrid stroke="#f5f5f5" />
-  <Line type="monotone" dataKey="uv" stroke="#ff7300" yAxisId={0} />
-  <Line type="monotone" dataKey="pv" stroke="#387908" yAxisId={1} />
-</LineChart>
+import React from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+
+const data = [
+    { name: 'Assignment 1', uv: 55, pv: 2400 },
+    { name: 'Assignment 2', uv: 53, pv: 1398 },
+    { name: 'Assignment 3', uv: 27, pv: 9800 },
+    { name: 'Assignment 4', uv: 36, pv: 3908 },
+    { name: 'Assignment 5', uv: 56, pv: 4800 },
+    { name: 'Assignment 6', uv: 37, pv: 3800 },
+    { name: 'Assignment 6', uv: 60, pv: 4300 },
+    { name: 'Assignment 7', uv: 34, pv: 4300 }
+  ];
+const Statatistic = () => {
+    return (
+        <div>
+    
+    <AreaChart width={1000} height={300} data={data}>
+    <XAxis dataKey="name" />
+    <YAxis />
+    <CartesianGrid strokeDasharray="3 3" />
+    <Tooltip />
+    <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+  </AreaChart>
+
+        </div>
+    );
+};
+
+export default Statatistic;
