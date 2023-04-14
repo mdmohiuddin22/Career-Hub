67b8594181941d9db77f7045135e45717813c191
Md.Mohiuddin <saddammd203@gmail.com>
2023-04-14 22:27:06 +0600

    add diploy

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -13,6 +13,13 @@ useEffect(()=> {
         <div>
           <h1 className='text-5xl font-bold text-center'>Featured Jobs</h1>
           <h3 className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</h3>
+          <div className="dropdown ml-20">
+  <label tabIndex={0} className="btn m-1 "><h1 >Filter By</h1></label>
+  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a>Onsite</a></li>
+    <li><a>Remote</a></li>
+  </ul>
+</div>
       <div className='grid grid-cols-2 gap-10 items-center ml-36 mt-10 mb-10 justify-center'>
       {
           features.map(feature => <FeaturedJob
